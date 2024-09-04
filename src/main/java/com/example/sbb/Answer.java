41f8b34c12fafd185af4d00d3d03f7b78dc0c51c
sik2 <bickwang123@gmail.com>
2024-09-04 11:26:16 +0900

    2-04

---
+++ src/main/java/com/example/sbb/Answer.java
@@ -0,0 +1,24 @@
+package com.example.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
