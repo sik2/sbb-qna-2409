b20c572a46eb72e15ecc9b0a7a8dc9f626600f21
sik2 <bickwang123@gmail.com>
2024-09-04 13:20:47 +0900

    2-05 find

--- src/test/java/com/example/sbb/SbbApplicationTests.java
+++ src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -13,7 +17,7 @@ class SbbApplicationTests {
 	QuestionRepository questionRepository;
 
 	@Test
-	void contextLoads() {
+	void testJpa() {
 		Question q1 = new Question();
 		q1.setSubject("sbb가 무엇인가요?sbb에 대해서 알고 싶습니다.");
 		q1.setContent("sbb에 대해서 알고 싶습니다.");
@@ -25,6 +29,25 @@ class SbbApplicationTests {
 		q2.setContent("id는 자동으로 생성되나요?");
 		q2.setCreateDate(LocalDateTime.now());
 		this.questionRepository.save(q2);  // 두번째 질문 저장
+	}
+
+	@Test
+	void testJpa2() {
+		List<Question> all = this.questionRepository.findAll();
+
+		assertEquals(2, all.size());
+
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+
+	@Test
+	void testJpa3() {
+		Optional<Question> oq = this.questionRepository.findById(1);
+		if(oq.isPresent()) {
+			Question q = oq.get();
+			assertEquals("sbb가 무엇인가요?", q.getSubject());
+		}
 	}
 
 }
