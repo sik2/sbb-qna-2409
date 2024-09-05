c29229a231b190ee3ece2370d37f74af6718c765
sik2 <bickwang123@gmail.com>
2024-09-05 11:39:57 +0900

    fix 2-05

--- src/test/java/com/example/sbb/SbbApplicationTests.java
+++ src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -25,7 +25,7 @@ class SbbApplicationTests {
 	@Test
 	void testJpa() {
 		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?sbb에 대해서 알고 싶습니다.");
+		q1.setSubject("sbb가 무엇인가요?");
 		q1.setContent("sbb에 대해서 알고 싶습니다.");
 		q1.setCreateDate(LocalDateTime.now());
 		this.questionRepository.save(q1);  // 첫번째 질문 저장
