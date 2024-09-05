f9df2e9d9b4db0bdb024c3ccb5c1fc3021c3f264
sik2 <bickwang123@gmail.com>
2024-09-05 10:54:11 +0900

    2-05 answerList

--- src/test/java/com/example/sbb/SbbApplicationTests.java
+++ src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -113,4 +113,16 @@ class SbbApplicationTests {
 		Answer a = oa.get();
 		assertEquals(2, a.getQuestion().getId());
 	}
+
+	@Test
+	void testJpa11() {
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+
+		List<Answer> answerList = q.getAnswerList();
+
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+	}
 }
