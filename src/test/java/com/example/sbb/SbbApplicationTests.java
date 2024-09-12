72f3f402b6a7ff803a605b2e976b462e73945fc9
sik2 <bickwang123@gmail.com>
2024-09-12 09:20:01 +0900

    페이징 기능 구현 - 대량 테스트 데이터 만들기

--- src/test/java/com/example/sbb/SbbApplicationTests.java
+++ src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -4,6 +4,7 @@ import com.example.sbb.answer.Answer;
 import com.example.sbb.answer.AnswerRepository;
 import com.example.sbb.question.Question;
 import com.example.sbb.question.QuestionRepository;
+import com.example.sbb.question.QuestionService;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,9 @@ class SbbApplicationTests {
 
 	@Autowired
 	AnswerRepository answerRepository;
+
+	@Autowired
+	QuestionService questionService;
 
 	@Test
 	void testJpa() {
@@ -133,4 +137,14 @@ class SbbApplicationTests {
 		assertEquals(1, answerList.size());
 		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
 	}
+
+	@Test
+	void testJpa12() {
+		for (int i = 1; i <= 300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String content = "내용무";
+			this.questionService.create(subject, content);
+		}
+	}
+
 }
