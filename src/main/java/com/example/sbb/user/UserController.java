a3d77cee989b622cf20313c34dd5b173838bc72c
sik2 <bickwang123@gmail.com>
2024-09-13 13:21:34 +0900

    중복아이디 예외 처리

--- src/main/java/com/example/sbb/user/UserController.java
+++ src/main/java/com/example/sbb/user/UserController.java
@@ -2,6 +2,7 @@ package com.example.sbb.user;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,9 +31,18 @@ public class UserController {
 
             return "signup_form";
         }
-        
-        this.userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword1());
-        
+        try {
+            this.userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword1());
+        } catch (DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자 입니다.");
+            return "signup_form";
+        } catch (Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup_form";
+        }
+
         return "redirect:/";
     }
 }
