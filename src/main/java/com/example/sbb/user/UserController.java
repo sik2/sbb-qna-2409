9678736daf17c47c8c851fcd96d8c60f77acfdec
sik2 <bickwang123@gmail.com>
2024-09-13 12:50:01 +0900

    PasswordEncoder를 @Bean으로 등록, 회원가입 기능 구현

---
+++ src/main/java/com/example/sbb/user/UserController.java
@@ -0,0 +1,33 @@
+package com.example.sbb.user;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup_form";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "signup_form";
+        }
+        // TODO: 패스워드 일치 여부 검증
+        
+        // TODO: 회원가입 처리
+        
+        return "redirect:/";
+    }
+}
--- src/main/java/com/example/sbb/user/UserService.java
+++ src/main/java/com/example/sbb/user/UserService.java
@@ -1,20 +1,20 @@
 package com.example.sbb.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder PasswordEncoder ;
 
     public SiteUser create(String username, String email, String password) {
         SiteUser user = new SiteUser();
         user.setUsername(username);
         user.setEmail(email);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        user.setPassword(passwordEncoder.encode(password));
+        user.setPassword(PasswordEncoder.encode(password));
         this.userRepository.save(user);
         return user;
     }
